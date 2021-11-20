da2741bdc664cda29effd7f7de5abbbee848c5cd
HinaChleck <hinachleck@gmail.com>
2021-11-20 15:35:29 +0500

    Добавьте файлы проекта.

---
+++ Lab5/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Сформировать одномерный массив из 7 элементов. Заполнить массив числами, вводимыми с клавиатуры,
+ * определить среднее арифметическое элементов.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array=new int[7];
+            float S = 0;
+            //Random random = new Random();   
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+               // Console.Write(array[i]);
+                S += array[i];
+
+            }
+            Console.WriteLine();
+            float average=S/array.Length;
+            Console.WriteLine("Среднее арифметическое массива = {0:f2}",average);    
+
+            Console.ReadKey();
+        }
+    }
+}
